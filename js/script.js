86ef9a2406fea3de01f405c0c1754d210ffbb446
Gizzle <gizzle.bautista@gmail.com>
2016-08-21 13:41:32 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,24 @@ $(function() {
 	});
 
 });
+
+function toggle_color(color1,color2,cycle_time,wait_time) {
+setInterval(function first_color() {
+    $("hero").animate({
+        backgroundColor: color1
+    }, 1000, function () {
+        setTimeout(change_color, wait_time);
+    });
+}, cycle_time);
+
+function change_color() {
+    $("hero").animate({
+        backgroundColor: color2
+    }, 1000, function () {
+        setTimeout(function () {}, wait_time);
+    });
+}
+}
+
+toggle_color('#61beb3','#7d4b68',8000,3000);
+});
